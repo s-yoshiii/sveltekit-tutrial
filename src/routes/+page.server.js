03f86bfebbd98f4fd72438e79f1bde96cc343cb8
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-19 10:02:44 +0900

    データの取得、一覧ページの作成

---
+++ src/routes/+page.server.js
@@ -0,0 +1,5 @@
+import { getList } from "../lib/microcms";
+export const load = async () => {
+  return await getList();
+};
+export const prerender = true;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  export let data;
+</script>
+
 <!-- Top Bar Nav -->
 <nav class="w-full py-4 bg-blue-800 shadow">
   <div
@@ -78,95 +82,34 @@
 <div class="container mx-auto flex flex-wrap py-6">
   <!-- Posts Section -->
   <section class="w-full md:w-2/3 flex flex-col items-center px-3">
-    <article class="flex flex-col shadow my-4">
-      <!-- Article Image -->
-      <a href="#" class="hover:opacity-75">
-        <img
-          src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
-        />
-      </a>
-      <div class="bg-white flex flex-col justify-start p-6">
-        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4"
-          >Technology</a
-        >
-        <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4"
-          >Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a
-        >
-        <p href="#" class="text-sm pb-3">
-          By <a href="#" class="font-semibold hover:text-gray-800"
-            >David Grzyb</a
-          >, Published on April 25th, 2020
-        </p>
-        <a href="#" class="pb-6"
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
-          porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis
-          dui porta volutpat. In sit amet posuere magna..</a
-        >
-        <a href="#" class="uppercase text-gray-800 hover:text-black"
-          >Continue Reading <i class="fas fa-arrow-right" /></a
-        >
-      </div>
-    </article>
-
-    <article class="flex flex-col shadow my-4">
-      <!-- Article Image -->
-      <a href="#" class="hover:opacity-75">
-        <img
-          src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
-        />
-      </a>
-      <div class="bg-white flex flex-col justify-start p-6">
-        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4"
-          >Automotive, Finance</a
-        >
-        <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4"
-          >Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a
-        >
-        <p href="#" class="text-sm pb-3">
-          By <a href="#" class="font-semibold hover:text-gray-800"
-            >David Grzyb</a
-          >, Published on January 12th, 2020
-        </p>
-        <a href="#" class="pb-6"
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
-          porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis
-          dui porta volutpat. In sit amet posuere magna..</a
-        >
-        <a href="#" class="uppercase text-gray-800 hover:text-black"
-          >Continue Reading <i class="fas fa-arrow-right" /></a
-        >
-      </div>
-    </article>
-
-    <article class="flex flex-col shadow my-4">
-      <!-- Article Image -->
-      <a href="#" class="hover:opacity-75">
-        <img
-          src="https://source.unsplash.com/collection/1346951/1000x500?sig=3"
-        />
-      </a>
-      <div class="bg-white flex flex-col justify-start p-6">
-        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4"
-          >Sports</a
-        >
-        <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4"
-          >Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a
-        >
-        <p href="#" class="text-sm pb-3">
-          By <a href="#" class="font-semibold hover:text-gray-800"
-            >David Grzyb</a
-          >, Published on October 22nd, 2019
-        </p>
-        <a href="#" class="pb-6"
-          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
-          porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis
-          dui porta volutpat. In sit amet posuere magna..</a
-        >
-        <a href="#" class="uppercase text-gray-800 hover:text-black"
-          >Continue Reading <i class="fas fa-arrow-right" /></a
-        >
-      </div>
-    </article>
+    {#each data.contents as content}
+      <article class="flex flex-col shadow my-4">
+        <!-- Article Image -->
+        <a href={content.id} class="hover:opacity-75">
+          <img src={content.eyecatch?.url} />
+        </a>
+        <div class="bg-white flex flex-col justify-start p-6">
+          <a
+            href={content.id}
+            class="text-blue-700 text-sm font-bold uppercase pb-4"
+            >{content.category.name}</a
+          >
+          <a
+            href={content.id}
+            class="text-3xl font-bold hover:text-gray-700 pb-4"
+            >{content.title}</a
+          >
+          <a href={content.id} class="pb-6"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis
+            iaculis dui porta volutpat. In sit amet posuere magna..</a
+          >
+          <a href={content.id} class="uppercase text-gray-800 hover:text-black"
+            >Continue Reading <i class="fas fa-arrow-right" /></a
+          >
+        </div>
+      </article>
+    {/each}
 
     <!-- Pagination -->
     <div class="flex items-center py-8">
